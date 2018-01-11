61263b53c74d8b0840cde8787ff0e038aed01e9a
AllenAng <32876202+AllenAng@users.noreply.github.com>
2018-01-11 15:41:18 +0800

    added variable

--- AMSA/app/src/main/java/com/ss174h/amsa/EnvCondition/ReviewEnv.java
+++ AMSA/app/src/main/java/com/ss174h/amsa/EnvCondition/ReviewEnv.java
@@ -50,6 +50,8 @@ public class ReviewEnv extends AppCompatActivity implements ParserResponseInterf
 
         String model = "ddff";
 
+        String ssds = "dfd";
+
         //new HtmlParser(this).execute("http://www.manutd.com/");
 
 
